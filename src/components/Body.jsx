fd45214e672dfc892bafe4b63dac2ed376535b8f
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-09-20 13:33:05 +0300

    add photo

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,20 +4,32 @@ const Body = () => {
   return (
     <>
       <div className="body-container">
-        <p className="text-name">I am</p>
-        <h1 className="name">Andrei Ghemusi</h1>
+        <p className="text-name">I'm</p>
+        <div className="test">
+          {" "}
+          <h1 className="name">Andrei <br /> Ghemusi</h1>
+          <img
+            className="ghemusi"
+          
+            src="photo2.jfif"
+            alt="Photo of Andrei Ghemusi"
+          />
+        </div>
         <p className="text-name">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error soluta
           eum sed dolores corrupti iste debitis dignissimos aspernatur commodi
           consequuntur nobis, expedita accusamus, magnam numquam odio eveniet
           molestias quidem veritatis! Lorem ipsum dolor sit amet consectetur,
           adipisicing elit.
-          <div className="text-name"> Perspiciatis explicabo sunt totam consectetur animi
-          saepe magnam culpa non? Nostrum libero voluptatibus non aut quasi
-          accusamus odit perferendis, velit doloremque porro?</div>
+          <div className="text-name">
+            {" "}
+            Perspiciatis explicabo sunt totam consectetur animi saepe magnam
+            culpa non? Nostrum libero voluptatibus non aut quasi accusamus odit
+            perferendis, velit doloremque porro?
+          </div>
         </p>
         <div className="projects">
-            <h2>Projects</h2>
+          <h2>Projects</h2>
         </div>
       </div>
     </>
---
+++ src/components/Fetch.jsx
@@ -0,0 +1,17 @@
+const Fetch =() =>{
+
+
+    // async function getGithub(){
+    // //     await fetch('https://api.github.com/users/Andrei-Ghemusi/repos')
+    // //     .then(response => response.json())
+    // //     .then(data => console.log(data))
+    // // }
+    // // getGithub()
+
+    return(
+        <>
+        </>
+    )
+}
+
+export default Fetch
