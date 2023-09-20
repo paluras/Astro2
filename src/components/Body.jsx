1e5299c8843639699030014810219f863e6cb21a
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-09-20 14:23:55 +0300

    Add links

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,26 +7,22 @@ const Body = () => {
         <p className="text-name">I'm</p>
         <div className="test">
           {" "}
-          <h1 className="name">Andrei <br /> Ghemusi</h1>
+          <h1 className="name">
+            Andrei <br /> Ghemusi
+          </h1>
           <img
             className="ghemusi"
-          
             src="photo2.jfif"
             alt="Photo of Andrei Ghemusi"
           />
         </div>
         <p className="text-name">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error soluta
-          eum sed dolores corrupti iste debitis dignissimos aspernatur commodi
-          consequuntur nobis, expedita accusamus, magnam numquam odio eveniet
-          molestias quidem veritatis! Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit.
-          <div className="text-name">
-            {" "}
-            Perspiciatis explicabo sunt totam consectetur animi saepe magnam
-            culpa non? Nostrum libero voluptatibus non aut quasi accusamus odit
-            perferendis, velit doloremque porro?
-          </div>
+          I am a dedicated and highly skilled QA Automation Tester with a
+          passion for ensuring the quality and reliability of software
+          applications. My expertise lies in leveraging cutting-edge tools and
+          technologies to streamline the testing process and deliver impeccable
+          results.
+         
         </p>
         <div className="projects">
           <h2>Projects</h2>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,24 @@
-
-
 interface Props {
   title: string;
   description: string;
   tehno: string;
+  url: string;
 }
 
-const Card = ({ title, description, tehno }: Props) => {
-
+const Card = ({ title, description, tehno, url }: Props) => {
   return (
-    <div id="myElement"  className={`container-card hover`}>
+    <a href={url} target="_blank" id="myElement" className={`container-card hover`}>
       <div className="container-top">
         <div className="container-left">
           <div className="title">{title}</div>
           <div className="tehnologies">{tehno}</div>
         </div>
-       <a href=""> <svg  className="hover"
+        <svg
+          className="hover theIcon"
           width="18"
           height="18"
           viewBox="0 0 18 18"
           fill="none"
-        
         >
           <path
             d="M5.25 12.75L12.75 5.25"
@@ -34,10 +32,10 @@ const Card = ({ title, description, tehno }: Props) => {
             strokeLinecap="round"
             strokeLinejoin="round"
           ></path>
-        </svg></a>
+        </svg>
       </div>
       <div className="description">{description}</div>
-    </div>
+    </a>
   );
 };
 
